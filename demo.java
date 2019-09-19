902deee0189d8efd9534be01414306b8e5adea04
priyanka das <priyankadas145565@gmail.com>
2019-09-19 07:28:29 +0530

    first commit

---
+++ demo.java
@@ -0,0 +1,13 @@
+public class demo
+  {
+    public static void main(String args[])
+   {
+       byte b= 12;
+        String s= "A";
+        String x= "a";
+        byte b1= Byte.parseByte(s);
+        System.out.println(b1);
+        String SS= Byte.toString(b);
+         System.out.println(SS);
+   }
+ }
